94812ba3bd53a46745c8a2c421fa1f496c243272
Jürgen Kleer <bluen@users.noreply.github.com>
2022-07-24 14:50:53 +0200

    Update keymap.c

--- keyboards/keychron/k3/keymaps/iso_seyahdoo/keymap.c
+++ keyboards/keychron/k3/keymaps/iso_seyahdoo/keymap.c
@@ -17,6 +17,23 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include QMK_KEYBOARD_H
 
+#include "keymap_user.h"
+#include "rgb_matrix_user.h"
+
+// clang-format off
+
+typedef union {
+  uint32_t raw;
+  struct {
+    bool caps_lock_light_tab :1;
+    bool caps_lock_light_alphas :1;
+    bool fn_layer_transparent_keys_off :1;
+    bool fn_layer_color_enable :1;
+  };
+} user_config_t;
+
+user_config_t user_config;
+
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
@@ -27,6 +44,24 @@ enum layer_names {
   WIN_BASE,
   WIN_FN
 };
+
+enum custom_keycodes {
+#ifdef VIA_ENABLE
+    KC_MISSION_CONTROL = USER00,
+#else
+    KC_MISSION_CONTROL = SAFE_RANGE,
+#endif
+    KC_LAUNCHPAD,
+    KC_LIGHT_TAB_TOGGLE,
+    KC_LIGHT_ALPHAS_TOGGLE,
+    KC_FN_LAYER_TRANSPARENT_KEYS_TOGGLE,
+    KC_FN_LAYER_COLOR_TOGGLE
+};
+
+#define KC_LTTOG KC_LIGHT_TAB_TOGGLE
+#define KC_LATOG KC_LIGHT_ALPHAS_TOGGLE
+#define KC_TKTOG KC_FN_LAYER_TRANSPARENT_KEYS_TOGGLE
+#define KC_FCTOG KC_FN_LAYER_COLOR_TOGGLE
 
 #define KC_TASK LGUI(KC_TAB)        // Task viewer
 #define KC_FLXP LGUI(KC_E)          // Windows file explorer
@@ -85,7 +120,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,                RGB_MOD    ,
       _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,                            RGB_RMOD   ,
       _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,                RGB_SPI    ,
-      _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,                _______,    RGB_SAI,    RGB_SPD    ,
+      _______,    _______,    KC_LTTOG,  KC_LATOG,   KC_TKTOG,   KC_FCTOG,    _______,    _______,    _______,    _______,    _______,    _______,                _______,    RGB_SAI,    RGB_SPD    ,
       _______,    _______,    _______,                                        EEPROM_RESET,                                 OSM(MOD_HYPR),_______,OSM(MOD_MEH),   RGB_HUD,    RGB_SAD,    RGB_HUI
   ),
 
@@ -135,7 +170,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,                RGB_M_P    ,
       _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,                            _______    ,
       _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,                _______    ,
-      _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,    _______,                _______,    RGB_SAI,    _______    ,
+      _______,    _______,    KC_LTTOG,  KC_LATOG,   KC_TKTOG,    KC_FCTOG,   _______,    _______,    _______,    _______,    _______,    _______,                _______,    RGB_SAI,    _______    ,
       _______,    _______,    _______,                                        EEPROM_RESET,                                   _______,    _______,    _______,    RGB_HUD,    RGB_SAD,    RGB_HUI
   ),
 };
@@ -163,10 +198,66 @@ bool dip_switch_update_user(uint8_t index, bool active) {
   return true;
 }
 
+void matrix_init_user(void) {
+    rgb_matrix_init_user();
+}
+
 void keyboard_post_init_user(void) {
-  // Customise these values to desired behaviour
-  // debug_enable = true;
-  // debug_matrix = true;
-  // debug_keyboard = true;
-  // debug_mouse = true;
+    user_config.raw = eeconfig_read_user();
+}
+
+void eeconfig_init_user(void) {
+    user_config.raw = 0;
+    user_config.caps_lock_light_tab = false;
+    user_config.caps_lock_light_alphas = false;
+    user_config.fn_layer_transparent_keys_off = true;
+    user_config.fn_layer_color_enable = false;
+    eeconfig_update_user(user_config.raw);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case KC_LIGHT_TAB_TOGGLE:
+            if (record->event.pressed) {
+                user_config.caps_lock_light_tab ^= 1; // bitwise xor to toggle status bit
+                eeconfig_update_user(user_config.raw);
+            } 
+            return false;  // Skip all further processing of this key
+        case KC_LIGHT_ALPHAS_TOGGLE:
+            if (record->event.pressed) {
+                user_config.caps_lock_light_alphas ^= 1;
+                eeconfig_update_user(user_config.raw);
+            }
+            return false;  // Skip all further processing of this key
+        case KC_FN_LAYER_TRANSPARENT_KEYS_TOGGLE:
+            if (record->event.pressed) {
+                user_config.fn_layer_transparent_keys_off ^= 1;
+                eeconfig_update_user(user_config.raw);
+            }
+            return false;  // Skip all further processing of this key
+        case KC_FN_LAYER_COLOR_TOGGLE:
+            if (record->event.pressed) {
+                user_config.fn_layer_color_enable ^= 1;
+                eeconfig_update_user(user_config.raw);
+            }
+            return false;  // Skip all further processing of this key
+        default:
+            return true;  // Process all other keycodes normally
+    }
+}
+
+bool get_caps_lock_light_tab(void) {
+    return user_config.caps_lock_light_tab;
+}
+
+bool get_caps_lock_light_alphas(void) {
+    return user_config.caps_lock_light_alphas;
+}
+
+bool get_fn_layer_transparent_keys_off(void) {
+    return user_config.fn_layer_transparent_keys_off;
+}
+
+bool get_fn_layer_color_enable(void) {
+    return user_config.fn_layer_color_enable;
 }
