058bb1ea319b372d8dfb8185f9924f613318b51f
Jürgen Kleer <bluen@users.noreply.github.com>
2022-07-24 14:51:56 +0200

    Update keymap.c

--- keyboards/keychron/k3/keymaps/iso_seyahdoo/keymap.c
+++ keyboards/keychron/k3/keymaps/iso_seyahdoo/keymap.c
@@ -34,17 +34,6 @@ typedef union {
 
 user_config_t user_config;
 
-// Each layer gets a name for readability, which is then used in the keymap matrix below.
-// The underscores don't mean anything - you can have a layer called STUFF or any other name.
-// Layer names don't all need to be of the same length, obviously, and you can also skip them
-// entirely and just use numbers.
-enum layer_names {
-  MAC_BASE = 0,
-  MAC_FN,
-  WIN_BASE,
-  WIN_FN
-};
-
 enum custom_keycodes {
 #ifdef VIA_ENABLE
     KC_MISSION_CONTROL = USER00,
